824c94320bacf1930bd7a7ec8db22438dae57ef4
Ing. Julian Holzschuh <julian.holzs@gmail.com>
2025-02-18 23:54:20 -0300

    incluye hasta context

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,20 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ItemCount from './itemCount';
 import { CartContext } from '../context/CartContext';
 
+
+
 const ItemDetail = ({item}) => {
+  const {cart, addItem} = useContext(CartContext) //Llamo a los elementos del CartContext
+  const [purchase, setPurchase] = useState(false)
+
   //console.log(item); //NO entendia por que no me cargaba el ITEM, por que cargaba el ITEM todavia vacio...
   if (!item || Object.keys(item).length === 0) {
     return <h2 className="text-center text-gray-500">Cargando producto...</h2>;
   }
+  
   const onAdd = (cantidad) => {
-    alert(`Agregaste ${cantidad} al carritoS`)
-    addItem(productDetail, cantidad)
+    setPurchase(true)
+    addItem(item, cantidad)// Hay que definir productDetail todavia...
+    
   }
   
  
-  const{cart,addItem} = useContext(CartContext)
   console.log(cart, 'carrito')
+
   return (
     <div className='flex justify-center m-4'>
       <div className='max-w-80 flex flex-col justify-center border-solid border-4 border-slate-300 rounded-2xl items-center p-4 bg-slate-100'>
@@ -25,7 +32,10 @@ const ItemDetail = ({item}) => {
             <p>AR$ {item.price}</p>
           </div>
           <p>Description: {item.description}</p>
-          <ItemCount stock={item.stock} onAdd={onAdd}/>
+          {!purchase 
+          ? <ItemCount stock={item.stock} onAdd={onAdd}/>
+          : <button>Ir al carrito</button>}
+          
       </div>
     </div>
   )
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 
 
 //Creo el contexto
-export const CartContext = createContext(); //lo eporto para poder usarlo fuera
+export const CartContext = createContext(); //lo exporto para poder usarlo fuera
 
 //declaro el proveedor
 export const CartProvider = ({children}) => {
@@ -15,10 +15,38 @@ export const CartProvider = ({children}) => {
 
         const addItem = (item, quantity) => {
             let compra = {...item, quantity} //esto es un spreed
-            setCart(compra)
+            //si existe, le sumo, si no existe agrego el producto a la lista
+            if(isInCart(item.id)){
+                //sumar cantidades, no quiero repetidos
+                const updatedCart = cart.map((prod)=>{
+                    if(prod.id === item.id){
+                        return {...prod, quantity:prod.quantity + quantity}
+                    }else{
+                        return prod
+                    }
+                })
+                setCart(updatedCart)
+            } else { //agrego el producto al carrito
+                setCart([...cart, compra])
+            }
         }
+
+        const removeItem = (id) =>{
+            setCart(cart.filter((item)=>item.id !== id)) //que filtra los item.ids distintos a los que le llegan por parametro id
+        }
+
+        //Funcion que vacia el array del carrito
+        const clear = () => {
+            setCart([])
+        }
+
+        //Funcion que chequea si x id esta incluido en el array del carrito, devuelve true o false
+        const isInCart = (id) => {
+            return cart.some((item)=> item.id === id)
+        }
+
     return(
-        <CartContext.Provider value={{cart, addItem}}> {/* etiqueta cartContext que cumple el rol de ser un Provider */}
+        <CartContext.Provider value={{cart, addItem, removeItem, clear}}> {/* etiqueta cartContext que cumple el rol de ser un Provider */}
             {children}
         </CartContext.Provider>
     )
